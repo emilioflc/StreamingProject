ec725f927a6c5cc2821027330b4ebe003e65d34e
Emilio <emilioflores@uees.edu.ec>
2023-03-01 22:13:34 -0600

    Entidades Plan, Pago, y Perfil

--- StreamingPlatform/src/main/java/com/eflores/StreamingPlatform/model/Perfil.java
+++ StreamingPlatform/src/main/java/com/eflores/StreamingPlatform/model/Perfil.java
@@ -1,14 +1,10 @@
 package com.eflores.StreamingPlatform.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
-
 import static jakarta.persistence.GenerationType.AUTO;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
@@ -19,26 +15,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
-
+public class Perfil {
     @Id @GeneratedValue(strategy = AUTO)
     private Long id;
 
-    private String nombres;
-    private String apellidos;
-    private LocalDate fechaNacimiento;
-
-    @Column(unique=true)
-    @NotEmpty(message = "Se debe proporcionar un correo para continuar.")
-    private String correo;
-    
-    private String password;
-    private Plan plan;
-    private String pathFotoPerfil;
-    private LocalDateTime vencimientoPlan;
     private Usuario usuarioCreador;
+    private String alias;
+    private Boolean mayorEdad;
     private LocalDateTime fechaCreacion;
     private Usuario usuarioModificador;
     private LocalDateTime fechaModificacion;
-
 }
--- StreamingPlatform/src/main/java/com/eflores/StreamingPlatform/model/Plan.java
+++ StreamingPlatform/src/main/java/com/eflores/StreamingPlatform/model/Plan.java
@@ -1,14 +1,10 @@
 package com.eflores.StreamingPlatform.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
-
 import static jakarta.persistence.GenerationType.AUTO;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
@@ -19,26 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario {
-
-    @Id @GeneratedValue(strategy = AUTO)
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
 
-    private String nombres;
-    private String apellidos;
-    private LocalDate fechaNacimiento;
-
-    @Column(unique=true)
-    @NotEmpty(message = "Se debe proporcionar un correo para continuar.")
-    private String correo;
-    
-    private String password;
-    private Plan plan;
-    private String pathFotoPerfil;
-    private LocalDateTime vencimientoPlan;
+    private String nombre;
+    private String descripcion;
+    private Integer maxPerfiles;
     private Usuario usuarioCreador;
     private LocalDateTime fechaCreacion;
     private Usuario usuarioModificador;
     private LocalDateTime fechaModificacion;
-
 }
