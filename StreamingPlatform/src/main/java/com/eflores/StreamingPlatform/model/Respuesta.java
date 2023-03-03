e8366f0a030e7e4210333e8a4c8b769cf133568e
Emilio <emilioflores@uees.edu.ec>
2023-03-02 22:10:24 -0600

    clase Respuesta mod

--- StreamingPlatform/src/main/java/com/eflores/StreamingPlatform/model/Respuesta.java
+++ StreamingPlatform/src/main/java/com/eflores/StreamingPlatform/model/Respuesta.java
@@ -5,6 +5,15 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
 public class Respuesta {
     protected LocalDateTime timeStamp;
     protected int codigoStatus;
